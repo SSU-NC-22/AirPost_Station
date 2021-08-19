65d68fed7c2dec87f766a1f82c339792235bb661
jongsoo <jsoone24@gmail.com>
2021-08-20 01:39:33 +0900

    running code run.py, Subscriber handling function, Mqtt connection function modified

---
+++ run.py
@@ -0,0 +1,64 @@
+from MQTT_Connection.MQTT_Connection import MQTT
+from Actuator.Handler import Handler
+from Sensors.GPSSensor import GPSSensor
+from Sensors.LightSensor import LightSensor
+from Sensors.TagSensor import TagSensor
+from Sensors.TempHumidSensor import TempHumidSensor
+import datetime
+import time
+import json
+
+broker = '58.230.119.87'
+port = 9708
+client_id = '0'
+
+
+gps = GPSSensor()
+tag = TagSensor()
+light = LightSensor()
+temp_humid = TempHumidSensor()
+
+
+handler = Handler()
+mqtt = MQTT(broker, port, client_id)
+mqtt.connect_mqtt()
+mqtt.subscribe("data/0", handler)
+#mqtt.subscribe("command/downlink/ActuatorReq/0", handler)
+
+
+while True:
+	if temp_humid.Read() == -1:
+		print("temp_humid read fail")
+	if gps.Read() == -1:
+		print("gps read fail")
+	if light.Read() == -1:
+		print("light read fail")
+	if tag.Read() ==-1:
+		print("tag read fail")
+		clear = None
+	else:
+		len(tag.detectedTags)
+	
+	msgs = {
+		"node_id": client_id,
+		"values": {
+			"temp": temp_humid.temperature,
+			"humid": temp_humid.humidity,
+			"light": light.data,
+
+			"lat": gps.data['lat'],
+			"lon": gps.data['lon'],
+			"alt": gps.data['alt'],
+
+			"clear": clear,
+			"status": 0,
+		},
+		"timestamp": str(datetime.datetime.now())
+	}
+	
+	msg = json.dumps(msgs)
+
+	mqtt.publish("data/0", msg)
+	print("publish: ", type(msg), msg, "\n")
+
+	time.sleep(1)
