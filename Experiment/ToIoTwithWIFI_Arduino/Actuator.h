0c43de02ecd5c870d6229d0c7b8f780c201de837
jongsoo <jsoone24@gmail.com>
2021-09-25 05:05:00 +0900

    mqtt sub, pub topic change, and some major changes

--- Experiment/ToIoTwithWIFI_Arduino/Actuator.h
+++
@@ -1,9 +0,0 @@
-struct Actuator{
-  int actuatorId;
-  int value[6];
-  unsigned long interval[6];
-  unsigned long previousMillis = 0;
-  short int running_index=0;
-  bool run  = false;
-  short values_len = 0;
-};
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from MQTT_Connection.MQTT_Connection import MQTT
+from paho.mqtt import client as mqtt_client
 from Actuator.Handler import Handler
 from Sensors.GPSSensor import GPSSensor
 from Sensors.LightSensor import LightSensor
@@ -13,52 +13,90 @@ port = 9708
 client_id = '0'
 
 
-gps = GPSSensor()
-tag = TagSensor()
-light = LightSensor()
-temp_humid = TempHumidSensor()
+class MQTT():
+	def __init__(self, broker, port, client_id):
+		self.broker = broker
+		self.port = port
+		self.client_id = client_id
+		self.client = mqtt_client.Client(self.client_id)
+
+	def connect_mqtt(self):
+		def on_connect(client, userdata, flags, rc):
+			if rc == 0:
+				print("Connected to MQTT Broker!")
+			else:
+				print("Failed to connect, return code %d\n", rc)
+
+		self.client.on_connect = on_connect
+		self.client.connect(self.broker, self.port)
+		self.client.loop_start()
+
+	def publish(self, topic, msg):  # input msg type : string
+		self.pub_topic = topic
+		result = self.client.publish(self.pub_topic, msg)
+
+		# result: [0, 1]
+		status = result[0]
+		if status == 0:
+			print(f"Send msg to topic `{self.pub_topic}`")
+		else:
+			print(f"Failed to send message to topic {self.pub_topic}")
+
+	def subscribe(self, topic, handler):
+		self.sub_topic = topic
+		self.handler = handler
+
+		def on_message(client, userdata, msg):  # return msg.payload
+			print(f"Received msg from `{msg.topic}` topic")
+			self.handler.run(msg.payload)
+
+		self.client.on_message = on_message
+		self.client.subscribe(self.sub_topic)
 
 
-handler = Handler()
-mqtt = MQTT(broker, port, client_id)
-mqtt.connect_mqtt()
-mqtt.subscribe("data/0", handler)
-#mqtt.subscribe("command/downlink/ActuatorReq/0", handler)
+if __name__ == "__main__":
+	gps = GPSSensor()
+	tag = TagSensor()
+	light = LightSensor()
+	temp_humid = TempHumidSensor()
 
+	handler = Handler()
+	mqtt = MQTT(broker, port, client_id)
+	mqtt.connect_mqtt()
+	mqtt.subscribe("command/downlink/ActuatorReq/"+client_id)
 
-while True:
-	if temp_humid.Read() == -1:
-		print("temp_humid read fail")
-	if gps.Read() == -1:
-		print("gps read fail")
-	if light.Read() == -1:
-		print("light read fail")
-	if tag.Read() ==-1:
-		print("tag read fail")
-		clear = None
-	else:
-		len(tag.detectedTags)
-	
-	msgs = {
-		"node_id": client_id,
-		"values": {
-			"temp": temp_humid.temperature,
-			"humid": temp_humid.humidity,
-			"light": light.data,
+	while True:
+		# sensor value exception handler
+		if temp_humid.Read() == -1:
+			print("temp_humid read fail")
+		if gps.Read() == -1:
+			print("gps read fail")
+		if light.Read() == -1:
+			print("light read fail")
+		if tag.Read() == -1:
+			print("tag read fail")
+			clear = None
+		else:
+			clear = len(tag.detectedTags)
 
-			"lat": gps.data['lat'],
-			"lon": gps.data['lon'],
-			"alt": gps.data['alt'],
+		msgs = {
+			"node_id": client_id,
+			"values": [
+				temp_humid.temperature,
+				temp_humid.humidity,
+				light.data,
 
-			"clear": clear,
-			"status": 0,
-		},
-		"timestamp": datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-	}
-	
-	msg = json.dumps(msgs)
+				gps.data['lat'],
+				gps.data['lon'],
+				gps.data['alt'],
 
-	mqtt.publish("data/0", msg)
-	print("publish: ", type(msg), msg, "\n")
+				clear
+			],
+			"timestamp": datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+		}
 
-	time.sleep(1)
+		msg = json.dumps(msgs)
+		mqtt.publish("data/"+client_id, msg)
+		print("publish: ", type(msg), msg, "\n")
+
+		time.sleep(1)
