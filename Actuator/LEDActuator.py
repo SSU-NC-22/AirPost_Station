eab2755dffc733caa7c7b3fda283be71579c76ea
jongsoo <jsoone24@gmail.com>
2021-09-25 05:19:44 +0900

    actuator code fix

--- Actuator/LEDActuator.py
+++ Actuator/LEDActuator.py
@@ -5,10 +5,14 @@ class LED():
 	def __init__(self, gpio=32):
 		self.gpio = gpio
 
-		GPIO.setwarnings(False)  # Ignore warning for now
-		GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
-		GPIO.setup(self.gpio, GPIO.OUT,
-		           initial=GPIO.LOW)  # Set pin 8 to be an output pin and set initial value to low (off)
+		# Ignore warning for now
+		GPIO.setwarnings(False) 
+		
+		# Use physical pin numbering 
+		GPIO.setmode(GPIO.BOARD)
+
+		# Set pin 8 to be an output pin and set initial value to low (off)
+		GPIO.setup(self.gpio, GPIO.OUT, initial=GPIO.LOW)  
 
 	def On(self):
 		GPIO.output(self.gpio, GPIO.HIGH)  # Turn on
