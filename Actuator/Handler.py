d7ab113795cab7ebf6a8189f422c784f4f0faa9f
jongsoo <jsoone24@gmail.com>
2021-09-26 00:45:15 +0900

    actuator fix

--- Actuator/Handler.py
+++ Actuator/Handler.py
@@ -1,5 +1,5 @@
 import json
-from LEDActuator import LED
+import RPi.GPIO as GPIO  # Import Raspberry Pi GPIO library
 
 
 class Handler():
@@ -12,3 +12,20 @@ class Handler():
             self.led.On()
         if msg['value'] == 0:
             self.led.Off()
+
+class LED():
+    def __init__(self, gpio=32):
+        self.gpio = gpio
+        # Ignore warning for now
+        GPIO.setwarnings(False) 
+        # Use physical pin numbering 
+        # GPIO.setmode(GPIO.BOARD)
+        GPIO.setmode(GPIO.BCM)
+        # Set pin 8 to be an output pin and set initial value to low (off)
+        GPIO.setup(self.gpio, GPIO.OUT, initial=GPIO.LOW)
+
+    def On(self):
+        GPIO.output(self.gpio, GPIO.HIGH)  # Turn on
+
+    def Off(self):
+        GPIO.output(self.gpio, GPIO.LOW)  # Turn off
